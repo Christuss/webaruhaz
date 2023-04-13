829ed44be08336b47d62ad0c135ed933b0ec6e3e
KSZBence <kunszekelybence@gmil.com>
2023-04-13 16:42:32 +0200

    rendezesek jo felkesz

--- admin.js
+++ admin.js
@@ -1,6 +1,9 @@
 import { ADATOK } from "./adatok.js";
 
 const MTBODY = $("#maintableb");
+let szamrendezett = false;
+let nevrendzett = false;
+
 
 $(function () {
     main();
@@ -8,25 +11,62 @@ $(function () {
 
 function main() {
     MTBODY.eq(0).html(tablaToltes());
-    $("#kor").click(korrendezes());
+    let nev = $("#nev");
+    let kor = $("#kor");
+    let faj = $("#faj");
+
+    nev.on("click", function(){
+        rendezes("n")
+    });
+
+    kor.on("click", function(){
+        rendezes("k")
+    });
+
+    faj.on("click", function(){
+        rendezes("f")
+    });
 }
 
 function tablaToltes() {
     let szoveg = "";
     for (let index = 0; index < ADATOK.length; index++) {
-        szoveg += `<tr><td>${ADATOK[index].nev}</td><td>${ADATOK[index].kor}</td><td>${ADATOK[index].fajta}</td></tr>`;
+        szoveg += `<tr><td>${ADATOK[index].nev}</td><td>${ADATOK[index].kor}</td><td>${ADATOK[index].fajta}</td><td>❌</td></tr>`;
     }
     return szoveg;
 }
 
 function korrendezes(){
-    let korrendezes = false;
-    if (!korrendezes) {
-        ADATOK.sort(function(a, b){return a-b});
-        korrendezes = true;
+    if (!szamrendezett) {
+        ADATOK.sort(function(a, b){return a.kor - b.kor});
+        szamrendezett = true;
         MTBODY.eq(0).html(tablaToltes());
     } else {
-        ADATOK.sort(function(a, b){return b-a});
-        korrendezes = false;
-    } 
+        ADATOK.reverse();
+        szamrendezett = false;
+        MTBODY.eq(0).html(tablaToltes());
+    }
+}
+
+function nevrendzes(){
+    if (!nevrendzett) {
+        ADATOK.sort(function(a, b){return a.nev - b.nev});
+        szamrendezett = true;
+        MTBODY.eq(0).html(tablaToltes());
+    } else {
+        ADATOK.reverse();
+        szamrendezett = false;
+        MTBODY.eq(0).html(tablaToltes());
+    }
+}
+
+function rendezes(rendezendo){
+    if (rendezendo === "n") {
+        ADATOK.sort(function(a, b){return a.nev < b.nev});
+        nevrendzet
+    } else if(rendezendo === "k"){
+        
+    }else{
+
+    }
 }
