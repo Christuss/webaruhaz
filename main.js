bf62f27a2a3d4f04351924642b2127dc5858d8c4
Christuss <szendefi.krisztofer@gmail.com>
2023-04-14 10:14:08 +0200

    modal, modalfeltolt(szam)

--- main.js
+++ main.js
@@ -5,16 +5,26 @@ $(function () {
 })
 
 function startUp() {
-    let szoveg = "";
     const DIV = $("body main div");
-    szoveg += kartyakDivbe();
-    DIV.eq(0).html(szoveg);
+    DIV.eq(0).html(kartyakDivbe());
+    const GOMB = $(".mod");
+    const MODAL = $("#modbod");
+    console.log(GOMB);
+    GOMB.on("click",function () {
+        MODAL.html(modalfeltolt(this.id));
+    })
 }
 
 function kartyakDivbe() {
     let szoveg = "";
     for (let index = 0; index < ADATOK.length; index++) {
-        szoveg += '<div class="card col-md-3"><div class="card-body"><h2 class="card-title">' + ADATOK[index].nev + '</h2><p class="card-body"><b>Kor:</b> ' + ADATOK[index].kor + '<br><b>Fajta: </b>' + ADATOK[index].fajta + '</p></div></div>';
+        szoveg += '<div class="card col-md-3"><div class="card-body"><h2 class="card-title">' + ADATOK[index].nev + '</h2><p><b>Kor:</b> ' + ADATOK[index].kor + '<br><b>Fajta: </b>' + ADATOK[index].fajta + '</p><button id= "' + index + '" type="button" class="btn btn-primary mod" data-bs-toggle="modal" data-bs-target="#myModal">Mutat</button></div></div>';
     }
+    return szoveg;
+}
+
+function modalfeltolt(szam) {
+    let szoveg = "";
+    szoveg += '<h2>' + ADATOK[szam].nev + '</h2><p><b>Kor:</b> ' + ADATOK[szam].kor + '<br><b>Fajta: </b>' + ADATOK[szam].fajta + '</p>';
     return szoveg;
 }
