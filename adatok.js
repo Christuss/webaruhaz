c35a21079745179312d00449ce16243e65e5ce73
KSZBence <kunszekelybence@gmil.com>
2023-04-14 10:00:56 +0200

    rendezes+torles kesz

--- adatok.js
+++ adatok.js
@@ -1,25 +1,30 @@
 export const ADATOK = [
     {
+        id: 1,
         nev:"Keksz",
         kor:5,
         fajta:"puli"
     },
     {
+        id: 2,
         nev:"Vilu",
         kor:2,
         fajta:"labrador"
     },
     {
+        id: 3,
         nev:"Bamba",
         kor:3,
         fajta:"csivava"
     },
     {
+        id: 4,
         nev:"Foltos",
         kor:6,
         fajta:"dalmata"
     },
     {
+        id: 5,
         nev:"Pista",
         kor:34,
         fajta:"homo sapiens"
